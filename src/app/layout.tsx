ca16e4d8d1a411afb4b7039633998b967ee4ac50
chaesunbak <coanstjd0422@gmail.com>
2026-02-17 00:04:33 +0900

    feat : 프리텐다트 폰트 적용
    - 가장 최적화된 방식인 next/font/local 방식으로 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
-
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
@@ -8,8 +7,11 @@ import { source } from "@/lib/source";
 
 import "./global.css";
 
-const inter = Inter({
-  subsets: ["latin"],
+const pretendard = localFont({
+  src: "../assets/fonts/pretendard/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
 });
 
 const baseOptions: BaseLayoutProps = {
@@ -23,7 +25,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
   const tree = source.getPageTree();
 
   return (
-    <html lang="ko" className={inter.className} suppressHydrationWarning>
+    <html lang="ko" className={pretendard.variable} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>
           <DocsLayout tree={tree} {...baseOptions}>
