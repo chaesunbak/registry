c2a27d114ff5f1dc0a433dbe1aafcba682eb95d5
chaesunbak <coanstjd0422@gmail.com>
2026-02-16 23:37:59 +0900

    chore : baseOptions 객체 루트레이아웃으로 이동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 
 import { Inter } from "next/font/google";
 import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 import { source } from "@/lib/source";
-import { baseOptions } from "@/lib/layout.shared";
 
 import "./global.css";
 
@@ -12,12 +12,21 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
+const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: "@chaesunbak/ui",
+  },
+  githubUrl: `https://github.com/chaesunbak/ui`,
+};
+
 export default function Layout({ children }: LayoutProps<"/">) {
+  const tree = source.getPageTree();
+
   return (
     <html lang="ko" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>
-          <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
+          <DocsLayout tree={tree} {...baseOptions}>
             {children}
           </DocsLayout>
         </RootProvider>
