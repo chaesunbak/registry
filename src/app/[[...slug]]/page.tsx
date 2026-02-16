f87bfc6baf0a10bfe93343ead95d6c5ecee53323
chaesunbak <coanstjd0422@gmail.com>
2026-02-17 01:57:07 +0900

    chore : page-actions 컴포넌트 제거

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -11,9 +11,6 @@ import { createRelativeLink } from "fumadocs-ui/mdx";
 
 import { getPageImage, source } from "@/lib/source";
 
-// import { LLMCopyButton, ViewOptions } from "@/components/ai/page-actions";
-// import { gitConfig } from "@/lib/layout.shared";
-
 export default async function Page(props: PageProps<"/[[...slug]]">) {
   const params = await props.params;
   const page = source.getPage(params.slug);
@@ -27,14 +24,7 @@ export default async function Page(props: PageProps<"/[[...slug]]">) {
       <DocsDescription className="mb-0">
         {page.data.description}
       </DocsDescription>
-      {/* <div className="flex flex-row gap-2 items-center border-b pb-6">
-        <LLMCopyButton markdownUrl={`${page.url}.mdx`} />
-        <ViewOptions
-          markdownUrl={`${page.url}.mdx`}
-          // update it to match your repo
-          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/content/docs/${page.path}`}
-        />
-      </div> */}
+
       <DocsBody>
         <MDX
           components={getMDXComponents({
