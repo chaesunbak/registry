d01aa64ac26be7c040895347adcc87040fee727b
chaesunbak <coanstjd0422@gmail.com>
2026-02-17 18:30:12 +0900

    feat : AutoTypeTable 컴포넌트 mdx-componenets에 추가

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,23 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import * as TabsComponents from "fumadocs-ui/components/tabs";
 import type { MDXComponents } from "mdx/types";
+import {
+  createFileSystemGeneratorCache,
+  createGenerator,
+} from "fumadocs-typescript";
+import { AutoTypeTable } from "fumadocs-typescript/ui";
+
+const generator = createGenerator({
+  cache: createFileSystemGeneratorCache(".next/fumadocs-typescript"),
+});
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...TabsComponents,
+    AutoTypeTable: (props) => (
+      <AutoTypeTable {...props} generator={generator} />
+    ),
     ...components,
   };
 }
